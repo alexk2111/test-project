4865ded126b8cddfebb64eeed6e8413975014bed
Alexandr Kucherenko <alexk211170@gmail.com>
2019-01-28 12:43:36 +0200

    Create migrations and models for catalogs

---
+++ app/Driver.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Driver extends Model
+{
+    //
+    const FIELD_ID = 'id';
+    const FIELD_NAME = 'name';
+    const FIELD_UPDATED_AT = 'updated_at';
+    const FIELD_CREATED_AT = 'created_at';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        self::FIELD_ID,
+        self::FIELD_NAME
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+}
