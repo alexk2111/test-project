f34c62c360cae11a32f6d9df0a8fa6a1d56c18c7
Alexandr Kucherenko <alexk211170@gmail.com>
2019-01-29 18:24:20 +0200

    Create Driver CRUD Creating frontend

---
+++ app/Http/Controllers/Api/DriversController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Driver;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Nexmo\Response;
+use Illuminate\Support\Facades\Validator;
+
+class DriversController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Return all drivers
+        return response()->json((Driver::get()), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //Create new drivers
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id = null)
+    {
+        //Update or create driver
+        if (!$driver=Driver::find($id)){
+            $driver = new Driver();
+        }
+
+        Validator::make($request->all(), [
+            Driver::FIELD_NAME => 'required',
+        ])->validate();
+        $driver->fill($request->all([
+            Driver::FIELD_NAME
+        ]));
+        $driver->save();
+        return response()->json(('success'), 200);;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Delete driver
+        Driver::destroy($id);
+        return response()->json(('success'), 200);;
+    }
+}
