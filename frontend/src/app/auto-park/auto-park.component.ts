6b84692f46f62357a174e1687038ba14e6afe779
Alexandr Kucherenko <alexk211170@gmail.com>
2019-02-15 18:01:38 +0200

    Filters

--- frontend/src/app/auto-park/auto-park.component.ts
+++ frontend/src/app/auto-park/auto-park.component.ts
@@ -2,6 +2,10 @@ import {Component, Input, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AutoParkService } from "../services/auto-park.service";
 import { AutoPark } from "../models/auto-park";
+import {TypeState} from "../models/type-states";
+import {TypeStatus} from "../models/type-statuses";
+import {TypeStatesService} from "../services/type-states.service";
+import {TypeStatusesService} from "../services/type-statuses.service";
 
 @Component({
   selector: 'app-auto-parks',
@@ -10,13 +14,19 @@ import { AutoPark } from "../models/auto-park";
 })
 export class AutoParkComponent implements OnInit {
   autoParks: Array<AutoPark>;
+  typeStates: Array<TypeState>;
+  typeStatuses: Array<TypeStatus>;
   error: string;
 
   constructor(
     private autoParkService: AutoParkService,
+    private typeStatesService: TypeStatesService,
+    private typeStatusesService: TypeStatusesService,
   ) { }
 
   ngOnInit() {
+    this.loadTypeStates();
+    this.loadTypeStatuses();
     this.loadAutoPark();
   }
 
@@ -34,7 +44,15 @@ export class AutoParkComponent implements OnInit {
     })
   }
 
-  viewLogAutoPark(id) {
+  loadTypeStates() {
+    this.typeStatesService.getTypeStates().subscribe((typeStates: Array<any>) => {
+      this.typeStates = typeStates.map(item => new TypeState(item));
+    })
+  }
 
+  loadTypeStatuses() {
+    this.typeStatusesService.getTypeStatuses().subscribe((typeStatuses: Array<any>) => {
+      this.typeStatuses = typeStatuses.map(item => new TypeStatus(item));
+    })
   }
 }
